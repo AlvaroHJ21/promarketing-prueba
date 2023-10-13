3978899c21b9ce29a33c2ee568dc25bc7000a343
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-10-13 16:30:13 -0500

    add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,59 @@
+import { cn } from '@/lib/util';
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: 'primary' | 'accent';
+  size?: 'lg';
+  variant?: 'contained' | 'outline';
+  iconPosition?: 'left' | 'right';
+  icon?: React.ReactNode;
+}
+
+export const Button = (props: Props) => {
+  const {
+    children,
+    className,
+    color = 'primary',
+    size = 'lg',
+    variant = 'contained',
+    iconPosition = 'left',
+    icon,
+    ...restProps
+  } = props;
+
+  return (
+    <button
+      className={cn(
+        //BASE CLASES
+        `uppercase py-[14px] px-8 font-bold rounded-lg flex gap-[10px] items-center border`,
+
+        //PRIMARY
+        color == 'primary' &&
+          variant != 'outline' &&
+          `bg-primary-900 border-primary-900 text-white hover:bg-primary-500 hover:border-primary-500 active:border-primary-900 disabled:bg-neutral-100 disabled:border-neutral-100 disabled:text-neutral-200`,
+
+        color == 'primary' &&
+          variant == 'outline' &&
+          `bg-transparent border-2 border-primary-900 text-primary-900 hover:bg-primary-100 active:bg-primary-500 active:text-white disabled:border-neutral-200 disabled:text-neutral-200`,
+
+        //ACCENT
+        color == 'accent' &&
+          variant != 'outline' &&
+          `bg-accent-900 border-accent-900 text-white hover:bg-accent-500 hover:border-accent-500 active:border-accent-900 disabled:bg-neutral-100 disabled:border-neutral-100 disabled:text-neutral-200`,
+
+        color == 'accent' &&
+          variant == 'outline' &&
+          `bg-transparent border-2 border-accent-900 text-accent-900 hover:bg-accent-100 active:bg-accent-500 active:text-white disabled:border-neutral-200 disabled:text-neutral-200`,
+
+        // ICON POSITION
+        iconPosition == 'right' && 'flex-row-reverse',
+
+        // ADDITIONAL CLASSES
+        className
+      )}
+      {...restProps}
+    >
+      {icon}
+      {children}
+    </button>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -10,11 +10,22 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'primary-900': '#091B50',
+        'primary-500': '#3C4D82',
+        'primary-100': '#E0E4EF',
+
+        'accent-900': '#FBBF24',
+        'accent-500': '#FFCA1C',
+        'accent-100': '#F7F3E3',
+
+        'neutral-200': '#909090',
+        'neutral-100': '#E8E8E8',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
