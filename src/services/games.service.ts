3d16a81298b1c5365a0fd10cd9d872adbb9cb782
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-10-14 12:23:17 -0500

    add game service

--- src/services/games.service.ts
+++ src/services/games.service.ts
@@ -1,7 +1,9 @@
 import type { Game } from '@/interfaces/Game';
 
+const GAME_API_URL = 'https://run.mocky.io/v3/a095fe75-f09e-45fa-967c-4b9c4c908fb3';
+
 export async function getGames(): Promise<Game[]> {
-  const resp = await fetch('https://run.mocky.io/v3/a095fe75-f09e-45fa-967c-4b9c4c908fb3');
+  const resp = await fetch(GAME_API_URL);
   const data = await resp.json();
   return data;
 }
