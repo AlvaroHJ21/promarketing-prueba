f3dd06d260210abd144d1ec3715067dfc5d54c53
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-10-14 14:58:31 -0500

    update order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,16 @@ export default async function Home() {
     <main className="max-w-screen-lg m-auto w-[90%]">
       <h1 className="text-center text-2xl uppercase my-8">Prueba Técnica Promarketing</h1>
 
-      <section>
+      <section className="">
+        <h2 className="my-8">Cards</h2>
+        <div className="grid grid-cols-2 w-fit gap-2">
+          {games.map((game) => (
+            <Card key={game.name} game={game} />
+          ))}
+        </div>
+      </section>
+
+      <section className="mb-8">
         <h2 className="my-8">Buttons</h2>
         <div className="flex gap-2 mb-2 flex-wrap">
           <Button icon={<FaChevronDown size={20} />} aria-label="dropdown" />
@@ -43,15 +52,6 @@ export default async function Home() {
           <Button variant="outline" color="accent" disabled>
             Button
           </Button>
-        </div>
-      </section>
-
-      <section>
-        <h2 className="my-8">Cards</h2>
-        <div className="grid grid-cols-2 w-fit gap-2">
-          {games.map((game) => (
-            <Card key={game.name} game={game} />
-          ))}
         </div>
       </section>
     </main>
